44a522f6aff62ba038aef26df4bbb787e6eae985
ugursoldan <ugursoldan@gmail.com>
2026-06-21 20:31:52 +0300

    5 dilde uzun özetli haberler

--- trigger-cron.sh
+++ trigger-cron.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s 'https://avrasya-bulteni-production.up.railway.app/api/cron?secret=***'
+curl -s 'https://avrasya-bulteni-production.up.railway.app/api/cron?secret=avrasya2024' 2>&1
